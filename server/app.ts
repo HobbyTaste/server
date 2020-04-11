343e1fd94cdf379829d30d249c9d80aac0bd748e
Maxim Utushkin <utushkin.ma@phystech.edu>
2020-04-11 14:52:06 +0300

    [HT-41] Added nginx balancer & deleted CORS headers

--- server/app.ts
+++ server/app.ts
@@ -39,18 +39,6 @@ app.use(cookieParser());
 app.use(fileUpload());
 */
 
-// CORS headers enabling
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    next();
-
-    app.options('*', (req, res) => {
-         res.header('Access-Control-Allow-Methods', 'GET, PATCH, PUT, POST, DELETE, OPTIONS');
-         res.send();
-    });
-})
-
 app.use(expressSession({
   secret: 'pugs do drugs',
   resave: false,
