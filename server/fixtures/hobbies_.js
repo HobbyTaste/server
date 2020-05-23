4332c089e5be97c4101a0ad42b72350b927b2891
ignat <iggy99@mail.ru>
2020-05-24 02:29:34 +0300

    worktime processing

--- server/fixtures/hobbies_.js
+++ server/fixtures/hobbies_.js
@@ -19,6 +19,7 @@ module.exports = (collection) => {
             .then(userList => getCollection('providers')
                 .then(providerList => getCollection('comments')
                     .then(commentList => Promise.all(hobbyList.map(async hobby => {
+<<<<<<< Updated upstream
                         const hobbyCommentsIds = commentList
                             .filter(comment => hobby.comments.includes(comment.temp_id))
                             .map(comment => comment._id);
@@ -31,12 +32,40 @@ module.exports = (collection) => {
                         const hobbyOwnerId = providerList
                             .find(provider => hobby.owner === provider.email)
                             ._id;
+=======
+                        const hobbyCommentsIds = hobby.comments
+                            ? commentList
+                                .filter(comment => hobby.comments.includes(comment.temp_id))
+                                .map(comment => comment._id)
+                            : [];
+                        const subscribersIds = hobby.subscribers
+                            ? userList
+                                .filter(user => hobby.subscribers.includes(user.email))
+                                .map(user => user._id)
+                            : [];
+                        const providerSubsIds = hobby.providerSubscribers
+                            ? providerList
+                                .filter(provider => hobby.providerSubscribers.includes(provider.email))
+                                .map(provider => provider._id)
+                            : [];
+                        const hobbyOwnerId = hobby.owner
+                            ? providerList
+                                .find(provider => hobby.owner === provider.email)
+                                ._id
+                            : providerList
+                                .find(provider => 'superprovider@test.com' === provider.email)
+                                ._id;
+                        const workTimeArray = typeof hobby.workTime === 'string'
+                            ? hobby.workTime.split(',')
+                            : hobby.workTime
+>>>>>>> Stashed changes
                         return collection.updateOne(hobby, {
                             $set: {
                                 "subscribers": subscribersIds,
                                 "providerSubscribers": providerSubsIds,
                                 "owner": hobbyOwnerId,
-                                "comments": hobbyCommentsIds
+                                "comments": hobbyCommentsIds,
+                                "workTime": workTimeArray
                                 }
                             });
                     }))))))
